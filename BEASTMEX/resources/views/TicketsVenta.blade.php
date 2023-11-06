b365e2d0b5c1879a2bbdce49bfcc7a699cb15245
JonaRBL <121039246@upq.edu.mx>
2023-11-05 18:06:51 -0600

    Se crea vista de Consulta tickets

--- BEASTMEX/resources/views/TicketsVenta.blade.php
+++ BEASTMEX/resources/views/TicketsVenta.blade.php
@@ -9,7 +9,7 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="input-group mb-3">
-                        <input type="search" class="form-control me-2 ms-auto" style="max-width: 200px;" placeholder="Buscar por fecha o cliente">
+                        <input type="search" class="form-control me-2 ms-auto" style="max-width: 200px;" placeholder="Buscar">
                         <button class="btn btn-outline-secondary" type="button">Buscar</button>
                     </div>
                     <table class="table table-bordered table-striped">
@@ -50,9 +50,6 @@
         </div>
         <div class="d-flex justify-content-between">
             <a class="btn btn-secondary" href="#"> < Regresar </a>
-            <div>
-                <button class="btn btn-danger">Descargar Ticket</button>                
-            </div>
         </div>
 </div>
 
