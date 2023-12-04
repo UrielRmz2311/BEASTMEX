51a4dc1e912e2c94feb1acc8342169a12960c964
UrielRmz2311 <125678140+UrielRmz2311@users.noreply.github.com>
2023-12-03 22:18:14 -0600

    Se crearon los lugares de trabajo

--- BEASTMEX/routes/web.php
+++ BEASTMEX/routes/web.php
@@ -1,4 +1,110 @@
 <?php
 
+//Impoortaciones
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Controlador;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Fin de importaciones
+
+//Rutas Alan
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Fin de rutas Alan
+
+//Rutas Joni
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Fin de rutas Joni
+
+//Rutas Jessy
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Fin de rutas Jessy
